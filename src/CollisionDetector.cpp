2614bba12678b3d2c66f0fc02df20bd800151b80
sekiya-mukai <mukaisekiya@gmail.com>
2026-09-06 05:38:22 +0000

    Modified Relative speed from km/h to m/s

--- src/CollisionDetector.cpp
+++ src/CollisionDetector.cpp
@@ -11,20 +11,26 @@ bool CollisionDetector::isCollisionRisk(
     const SensorData& sensorData) const
 {
     // 相対速度
-    const double relativeSpeed =
+    const double relativeSpeedKmh =
         sensorData.egoSpeed -
         sensorData.frontSpeed;
 
-    // 前方車両の方が速い場合は安全
-    if (relativeSpeed <= 0.0)
+// relativeSpeed < 0 : CD-BV-001  -0.1
+// relativeSpeed = 0 : CD-BV-002   0.0
+// relativeSpeed > 0 : CD-BV-003  +0.1
+
+
+    if (relativeSpeedKmh <= 0.0)
     {
         return false;
     }
 
-    // TTC計算
-    const double ttc =
-        sensorData.distance / relativeSpeed;
+    const double relativeSpeedMs =
+        relativeSpeedKmh / 3.6;
 
-    // TTCが閾値未満なら危険
+    const double ttc =
+        sensorData.distance / relativeSpeedMs;
+
     return ttc < TTC_THRESHOLD_S;
+
 }
